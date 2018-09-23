b0e1b746170f06e3eb586e03d6dfd72f5b515f47
GeoSyah <43498342+GeoSyah@users.noreply.github.com>
2018-09-23 14:10:00 +0700

    Add files via upload

---
+++ src/PBO2_10117081_Latihan7_Integer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * 10117081 IF-2
+ * Menampilkan cara penulisan bilangan bulat di Java
+ */
+public class PBO2_10117081_Latihan7_Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
